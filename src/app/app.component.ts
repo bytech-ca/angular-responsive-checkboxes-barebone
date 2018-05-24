190a6eae7dec999573e35f1ebd3c3d7c316ae895
Sławomir Borkowski <canadian@bytech.ca>
2018-05-24 13:35:22 +0200

    Added documentationa and renamed a couple functions to clearly describe their job

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
 
 export class AppComponent {
 
-  // Our dummy data - items where selected = true will be selected by default
+  // Our dummy data - items where selected = true will be pre-selected
 
   user = { skills: [
     { name: 'Angular', selected: false },
@@ -23,7 +23,7 @@ export class AppComponent {
 
   constructor(private formBuilder: FormBuilder) {
     this.checkboxForm = this.formBuilder.group({
-      skills: this.buildSkills()
+      skills: this.buildFormControlsForSkills()
     });
   }
 
@@ -31,13 +31,30 @@ export class AppComponent {
     return this.checkboxForm.get('skills');
   }
 
-  buildSkills() {
+  buildFormControlsForSkills() {
+
+    // .map high-function iterates the skills object, creating a form control for each
+
+    // ECMA6's fat arrow function has an implicit return statement when used on single line
+    // The easier to understand, full syntax version can be found at https://tinyurl.com/ya3oydpn
+
+    // Brackets in (skill) are optional. I use them to remember that this is an anonymous function
+
+    // For a great explanation of the power of .map higher function and the fat arrow
+    // function, see https://tinyurl.com/znh2tem.
+
     const arr = this.user.skills.map((skill) => this.formBuilder.control(skill.selected));
     return this.formBuilder.array(arr);
+
   }
 
-  updateSkills(i) {
+  toggleSkill(i) {
+
+    // .selected object is of type boolean, and as such can be toggled simply
+    // by setting it to what it's not
+
     this.user.skills[i].selected = !this.user.skills[i].selected;
+
   }
 
 }
